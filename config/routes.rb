d41ddc18d2adba7efd6a6245d1f41c30ad14b5d2
Jendnice <Pipeline38@hotmail.com>
2019-12-02 23:11:32 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
   # Add your routes here
+
+  match '/auth/:provider/callback', to: 'sessions#create', via: [:get, :post]
+
+  get '/welcome', to: 'welcome#home'
+
+   root to: "welcome#home"
+  # get '/', to: 'welcome#home'
+
 end
+
+
+
+#  https://localhost:3000/auth/facebook/callback
+#  http://localhost:3000/auth/github/callback
